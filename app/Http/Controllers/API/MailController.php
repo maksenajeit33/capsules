ccd87b7a9da8fe9159f5352b39713a142e3c856e
maksenajeit33 <dragongreen64@gmail.com>
2021-07-02 16:30:28 +0000

    Added MailController.php to send mails

---
+++ app/Http/Controllers/API/MailController.php
@@ -0,0 +1,31 @@
+<?php
+
+namespace App\Http\Controllers\API;
+
+use App\Http\Controllers\Controller;
+use App\Mail\SendRegistrationMail;
+use Illuminate\Support\Facades\Mail;
+
+class MailController extends Controller
+{
+    // SEND REGISTRATION VERIFICATION CODE
+    public function mailRegistrationVerify($name, $email, $code, $api_key)
+    {
+        $details = [
+            'name' => $name,
+            'code' => $code,
+        ];
+
+        if($api_key == env("API_KEY_N")) {
+            $details['logo'] = asset('images/logo_n.png');
+            $details['team'] = 'Capsule application team';
+            $details['app'] = 'Capsule';
+        } else if($api_key == env("API_KEY_O")) {
+            $details['logo'] = asset('images/logo_o.png');
+            $details['team'] = 'TG Focus application team';
+            $details['app'] = 'TG Focus';
+        }
+
+        Mail::to($email)->send(new SendRegistrationMail($details));
+    }
+}

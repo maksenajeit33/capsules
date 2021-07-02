99618f17bbd35ff6f020011bb3ca16326900ae48
maksenajeit33 <dragongreen64@gmail.com>
2021-07-02 16:33:52 +0000

    Modified AuthController for send a registration verify email address and check if the email is veried

--- app/Http/Controllers/API/AuthController.php
+++ app/Http/Controllers/API/AuthController.php
@@ -2,16 +2,17 @@
 
 namespace App\Http\Controllers\API;
 
-use App\Http\Controllers\Controller;
+use App\Http\Controllers\API\MailController;
 use App\Http\Traits\SendResponse;
+use App\Http\Traits\VerifyCode;
 use App\Models\User;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Hash;
 use Illuminate\Support\Facades\Validator;
 
-class AuthController extends Controller
+class AuthController extends MailController
 {
-    use SendResponse;
+    use SendResponse, VerifyCode;
 
     // REGISTER USER
     public function register(Request $request)
@@ -19,7 +20,6 @@ class AuthController extends Controller
         // Validate the request
         $validator = Validator::make($request->all(), [
             'name' => 'required',
-            'email' => 'required|email|unique:users,email',
             'email' => 'required|email|unique:users,email',
             'password' => 'required|min:8',
             'confirm_password' => 'required|same:password'
@@ -29,15 +29,22 @@ class AuthController extends Controller
         if($validator->fails())
             return $this->sendResponseError('Please validate the errors', $validator->errors(), 400);
 
+        // Get a generate number to send it in email and stored in DB
+        $code = $this->generate();
+
         // Create user and token
         $value = $request->all();
         $value['password'] = Hash::make($value['password']);
+        $value['code_verify'] = $code;
         $user = User::create($value);
         $result['name'] = $user->name;
         $result['token'] = $user->createToken('user@user')->accessToken;
 
+        // Send a mail to verify the registration
+        $this->mailRegistrationVerify($user->name, $user->email, $code, $request->api_key);
+
         // Send the response
-        return $this->sendResponseData($result, 'Register successful', 201);
+        return $this->sendResponseData($result, 'Please confirm your email address', 202);
     }
 
     // LOGIN USER
@@ -59,14 +66,23 @@ class AuthController extends Controller
 
         // Check from the password
         if(!Hash::check($value['password'], $user->password))
-            return $this->sendResponseError('Login Failed', '', 400);
+            return $this->sendResponseError('Login Failed', '', 406);
 
         // Create token
         $result['name'] = $user->name;
         $result['token'] = $user->createToken('user@user')->accessToken;
 
+        // Set a message to send it to user
+        if($user->hasVerifiedEmail()) {
+            $message = 'Login Successful';
+            $code = 202;
+        } else {
+            $message = 'Email must be verify';
+            $code = 403;
+        }
+
         // Send the response
-        return $this->sendResponseData($result, 'Login successful', 200);
+        return $this->sendResponseData($result, $message, $code);
     }
 
     // LOGOUT USER
@@ -80,6 +96,6 @@ class AuthController extends Controller
         $refreshTokenRepository->revokeRefreshTokensByAccessTokenId($request->user()->token()->id);
 
         // Send the response
-        return $this->sendResponseMessage('Logout successful', 200);
+        return $this->sendResponseMessage('Logout Successful', 200);
     }
 }

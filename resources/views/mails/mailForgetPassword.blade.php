a8b3bd904f4af4d61eb2e7da1b032fd655fc1b15
maksenajeit33 <dragongreen64@gmail.com>
2021-07-11 19:01:13 +0000

    Modified email HTML template

--- resources/views/mails/mailForgetPassword.blade.php
+++ resources/views/mails/mailForgetPassword.blade.php
@@ -11,11 +11,11 @@
             </div>
             <div style="background-color: #fff;border-radius: 5px;box-shadow: 0px 0px 15px 0px #9494941a;padding: 10px 30px;">
                 <h3>Hi, {{$details['name']}}!</h3>
-                <p style="margin: 8px 0 28px 0;">You are receiving this email because we received a password reset request for your account.</p>
+                <p style="margin: 8px 0 28px 0;">Please enter the code below to verify your email address.</p>
                 <div>
                     <input type="text" id="code" readonly value="{{$details['code']}}" style="margin: auto;display: block;cursor: pointer;border: none;background-color: #2d3748;color: #fff;font-size: 16px;padding: 15px 25px;border-radius: 5px;width: 110px;outline: none;margin-bottom: 30px;text-align: center;position: relative;">
                 </div>
-                <p style="margin: 20px 0 15px 0;">If you did not create an account, no further action is required.</p>
+                <p style="margin: 20px 0 15px 0;">You are receiving this email because we received a password reset request for your account.</p>
                 <p>Regards,<br>{{$details['team']}}</p>
             </div>
             <p style="text-align:center;padding:30px 0;color:#555;font-size:15px">&copy; 2021 {{$details['app']}}. All rights reserved.</p>
